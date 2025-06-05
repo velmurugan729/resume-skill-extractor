7af9708607826aec1b4471b4ca9e1028eed1059f
Velmurugan S <147385854+velmurugan729@users.noreply.github.com>
2025-06-06 00:31:31 +0530

    config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,10 @@
+import os
+from pydantic_settings import BaseSettings # type: ignore
 
+class Settings(BaseSettings):
+    llm_api_key: str = os.getenv("OPENAI_API_KEY", "")
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
