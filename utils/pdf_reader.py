1672d31dd9b609e707141786f29e3f4ea6925af8
Velmurugan S <147385854+velmurugan729@users.noreply.github.com>
2025-06-05 19:05:16 +0530

    pdf_reader.py

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF
 
-def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
+def extract_text_from_pdf(pdf_file):
+    doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
     text = ""
     for page in doc:
         text += page.get_text()
