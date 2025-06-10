2fa14038d1854fd2136054107bdd6f740c7fb211
Velmurugan S <147385854+velmurugan729@users.noreply.github.com>
2025-06-10 12:11:27 +0530

    config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,17 @@
 import os
+import spacy 
 
 class Settings:
     def __init__(self):
-        self.llm_api_key = os.getenv("OPENAI_API_KEY", "")
-        
+        self.APP_NAME = "Resume Parser extractor"
+        self.OPENAI_KEY = os.getenv("OPENAI_API_KEY", "")
+        self.USE_AI = True
+        self.DEBUG = False
+        # Load spaCy model
+        try:
+            self.nlp = spacy.load("en_core_web_lg")
+        except:
+            os.system("python -m spacy download en_core_web_lg")
+            self.nlp = spacy.load("en_core_web_lg")
+
 settings = Settings()
